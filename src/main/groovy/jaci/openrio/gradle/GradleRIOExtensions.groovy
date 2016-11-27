79ab1f625ebf3f9f4d6ac2f52f52254a6d4d2aac
Jaci R <jaci.brunning@gmail.com>
2016-11-27 21:15:26 +0800

    Add ability to specify WPI version/branch. Close #14

--- src/main/groovy/jaci/openrio/gradle/GradleRIOExtensions.groovy
+++ src/main/groovy/jaci/openrio/gradle/GradleRIOExtensions.groovy
@@ -3,5 +3,10 @@ class GradleRIOExtensions {
   String rioIP = "{DEFAULT}"
   String robotClass = "org.usfirst.frc.team0000.Robot"
   String deployFile = "FRCUserProgram.jar"
+
+  String wpilib_version = "+"
+  String ntcore_version = "+"
+  String wpi_branch = "release"
+
   def deployers = []
 }
--- src/main/groovy/jaci/openrio/gradle/WPIProvider.groovy
+++ src/main/groovy/jaci/openrio/gradle/WPIProvider.groovy
@@ -24,20 +24,22 @@ public class WPIProvider {
   public static void addWPILibraries(Project project) {
     project.repositories.maven {
       it.name = "WPI"
-      it.url = "http://first.wpi.edu/FRC/roborio/maven/release"
+      it.url = "http://first.wpi.edu/FRC/roborio/maven/" + project.gradlerio.wpi_branch
     }
     
     project.getConfigurations().maybeCreate("wpi_source")
     
     def deps = project.dependencies
+    def wpi_version = project.gradlerio.wpilib_version
+    def nt_version = project.gradlerio.ntcore_version
     
-    deps.add("compile", 'edu.wpi.first.wpilib.networktables.java:NetworkTables:+:desktop')
-    deps.add("compile", 'edu.wpi.first.wpilib.networktables.java:NetworkTables:+:arm')
+    deps.add("compile", 'edu.wpi.first.wpilib.networktables.java:NetworkTables:' + nt_version + ':desktop')
+    deps.add("compile", 'edu.wpi.first.wpilib.networktables.java:NetworkTables:' + nt_version + ':arm')
     
-    deps.add("compile", 'edu.wpi.first.wpilibj:wpilibJavaFinal:+')
+    deps.add("compile", 'edu.wpi.first.wpilibj:wpilibJavaFinal:' + wpi_version)
     
-    deps.add("wpi_source", 'edu.wpi.first.wpilib.networktables.java:NetworkTables:+:sources')
-    deps.add("wpi_source", 'edu.wpi.first.wpilibj:wpilibJavaFinal:+:sources')
+    deps.add("wpi_source", 'edu.wpi.first.wpilib.networktables.java:NetworkTables:' + nt_version + ':sources')
+    deps.add("wpi_source", 'edu.wpi.first.wpilibj:wpilibJavaFinal:' + wpi_version + ':sources')
   }
 
   public static boolean isToast() {
