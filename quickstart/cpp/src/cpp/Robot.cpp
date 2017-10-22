46a2097b4f814e08f737fa0ce831c84ba7926086
Jaci Brunning <jaci.brunning@gmail.com>
2017-10-22 17:58:10 +0800

    Add quickstart for Java and C++

---
+++ quickstart/cpp/src/cpp/Robot.cpp
@@ -0,0 +1,27 @@
+#include "WPILib.h"
+
+using namespace frc;
+
+// From MyHeader.h
+int add(int a, int b) {
+    return a + b;
+}
+
+class Robot: public IterativeRobot {
+public:
+    Robot() { }
+
+    void RobotInit() { }
+
+    void DisabledInit() { }
+    void AutonomousInit() { }
+    void TeleopInit() { }
+    void TestInit() { }
+
+    void DisabledPeriodic() { }
+    void AutonomousPeriodic() { }
+    void TeleopPeriodic() { }
+    void TestPeriodic() { }
+};
+
+START_ROBOT_CLASS(Robot)
