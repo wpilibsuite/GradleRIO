177d23da5ec115c1bdc895c0f4d0d1caf80827b8
Jaci Brunning <jaci.brunning@gmail.com>
2017-10-22 14:19:46 +0800

    Separate dependencies depending on active plugins

--- src/main/groovy/jaci/openrio/gradle/wpi/WPIPlugin.groovy
+++ src/main/groovy/jaci/openrio/gradle/wpi/WPIPlugin.groovy
@@ -1,19 +1,27 @@
 package jaci.openrio.gradle.wpi
 
-import jaci.openrio.gradle.wpi.dependencies.WPIDependenciesPlugin
+import jaci.openrio.gradle.wpi.dependencies.WPIJavaDeps
+import jaci.openrio.gradle.wpi.dependencies.WPINativeDeps
 import jaci.openrio.gradle.wpi.dependencies.WPIToolsPlugin
 import jaci.openrio.gradle.wpi.toolchain.WPIToolchainPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
+import org.gradle.nativeplatform.plugins.NativeComponentPlugin
 
 // TODO: Make @CompileStatic
 class WPIPlugin implements Plugin<Project> {
     void apply(Project project) {
         WPIExtension wpiExtension = project.extensions.create("wpi", WPIExtension, project)
 
-        project.pluginManager.apply(WPIDependenciesPlugin)
-        project.pluginManager.apply(WPIToolchainPlugin)
+        project.pluginManager.apply(WPIJavaDeps)
+
+        // Don't bother adding native stuff if we're not a native project
+        project.plugins.matching { it instanceof NativeComponentPlugin }.all {
+            project.pluginManager.apply(WPINativeDeps)
+            project.pluginManager.apply(WPIToolchainPlugin)
+        }
+
         project.pluginManager.apply(WPIToolsPlugin)
 
         project.task("wpi") { Task task ->
