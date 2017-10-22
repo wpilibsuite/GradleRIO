19fb05f3bdc3dca3044d40f8f50a23716b0ef501
Jaci Brunning <jaci.brunning@gmail.com>
2017-10-22 13:09:39 +0800

    Add local test, update to Gradle 4.2.1

---
+++ _localtest/src/main/java/test/MyClass.java
@@ -0,0 +1,8 @@
+package test;
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class MyClass extends IterativeRobot {
+    public void robotInit() {
+        System.out.println("Hello World");
+    }
+}
