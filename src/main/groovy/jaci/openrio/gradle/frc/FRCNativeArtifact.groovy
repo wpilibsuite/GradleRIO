b3266b53ddf1e56fabf0af615410f1f487249daf
Jaci Brunning <jaci.brunning@gmail.com>
2018-01-16 21:03:05 +0800

    Add new remote debugging framework

--- src/main/groovy/jaci/openrio/gradle/frc/FRCNativeArtifact.groovy
+++ src/main/groovy/jaci/openrio/gradle/frc/FRCNativeArtifact.groovy
@@ -4,7 +4,9 @@ import groovy.transform.CompileStatic
 import jaci.gradle.PathUtils
 import jaci.gradle.deploy.DeployContext
 import jaci.gradle.deploy.artifact.NativeArtifact
+import jaci.openrio.gradle.wpi.toolchain.WPIToolchainPlugin
 import org.gradle.api.Project
+import org.gradle.internal.os.OperatingSystem
 
 import java.nio.file.Paths
 
@@ -55,24 +57,30 @@ class FRCNativeArtifact extends NativeArtifact {
         }
 
         if (debug) {
-            def outfile = new File(project.buildDir, "debug/${name}.gdbcommands")
+            def gdbfile = new File(project.buildDir, "debug/${name}.gdbcommands")
+            def isWin = OperatingSystem.current().isWindows()
+            def cmdfile = new File(project.buildDir, "debug/${name}${isWin ? ".bat" : ""}")
 
             ctx.logger().log("====================================================================")
             ctx.logger().log("DEBUGGING ACTIVE ON PORT ${debugPort}!")
-            ctx.logger().log("Launch debugger with gdb -ix='${Paths.get(project.rootDir.toURI()).relativize(Paths.get(outfile.toURI()))}'")
-            ctx.logger().log("NOTE: If you are running GDB on multiple targets, you will have to run")
-            ctx.logger().log("      (gdb) target remote host:${debugPort}")
+            ctx.logger().log("Launch debugger with ${isWin ? "" : "./"}${Paths.get(project.rootDir.toURI()).relativize(Paths.get(cmdfile.toURI())).toString()}")
             ctx.logger().log("manually in case of conflicts.")
             ctx.logger().log("====================================================================")
 
             def init_commands = [
                 'set gnutarget elf32-littlearm',
-                "file ${_nativefile.absolutePath}".toString(),
+                "file ${_nativefile.absolutePath.replaceAll("\\\\", "/")}".toString(),
                 "target remote ${ctx.selectedHost()}:${debugPort}".toString()
             ]
 
-            outfile.parentFile.mkdirs()
-            outfile.text = init_commands.join('\n')
+            def gdbpath = new File(WPIToolchainPlugin.toolchainInstallDirectory(), "bin/arm-frc-linux-gnueabi-gdb").absolutePath
+            def cmdline = [
+                "${isWin ? "" : "./"}${gdbpath}${isWin ? ".exe" : ""}", "-ix=\"${gdbfile.absolutePath.replaceAll("\\\\", "/")}\""
+            ]
+
+            gdbfile.parentFile.mkdirs()
+            gdbfile.text = init_commands.join('\n')
+            cmdfile.text = cmdline.join(" ")
         }
     }
 }
