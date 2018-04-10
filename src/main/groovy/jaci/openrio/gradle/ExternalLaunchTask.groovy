864f65f9d90c7d740cc01d293835a8e6595e4479
Jeremy White <jwhite@codeweavers.com>
2018-04-10 10:42:42 -0500

    A set of changes to enable the simulation targets (#88)
    * Use the correct artifactGroupId for halsim-ds-nt.
    
    * Add the ability to configure an ExternalLaunchTask as headless.
    
    This allows us a poor man's launch by printing the script name.
    
    * Use the launch by script to enable the simulation targets.
    
    * The headless project property has to be checked during
    the task, not at object construction, so partially
    revert the change made earlier.
    
    Make the new functionality tied to a scriptOnly flag,
    rather that conflate the two concepts, that seems
    cleaner as well.
    
    * On windows, you need the working directory to be the library
    directory, not the exe install location.

--- src/main/groovy/jaci/openrio/gradle/ExternalLaunchTask.groovy
+++ src/main/groovy/jaci/openrio/gradle/ExternalLaunchTask.groovy
@@ -13,6 +13,7 @@ class ExternalLaunchTask extends DefaultTask {
     private def _withBuilderClosures = [] as List<Closure>
     def environment = [:] as Map<String, String>
     def persist = false
+    def scriptOnly = false
     def workingDir = null as File
 
     Process launch(String... cmd) {
@@ -63,7 +64,7 @@ class ExternalLaunchTask extends DefaultTask {
             }
         }
 
-        if (project.hasProperty('headless')) {
+        if (scriptOnly || project.hasProperty('headless')) {
             println "Commands written to ${file.absolutePath}! Run this file."
             return null;
         } else {
