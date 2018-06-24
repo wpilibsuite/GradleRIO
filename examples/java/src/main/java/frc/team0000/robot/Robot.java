63b7f0bab63a02c791a92f7c0cd184415d32b051
Jaci Brunning <jaci.brunning@gmail.com>
2018-06-24 15:32:31 +0800

    Update examples

---
+++ examples/java/src/main/java/frc/team0000/robot/Robot.java
@@ -0,0 +1,10 @@
+package frc.team0000.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class Robot extends IterativeRobot {
+    @Override
+    public void robotInit() {
+        System.out.println("Hello World! Beep Boop!");
+    }
+}
