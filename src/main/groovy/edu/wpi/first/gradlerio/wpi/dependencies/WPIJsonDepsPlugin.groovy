a5ddd9597d04ea63aa63f516e7024b8365b3df8b
Thad House <thadhouse1@gmail.com>
2018-09-06 11:23:52 -0700

    Adds javaVendorLibraries function for dependencies

--- src/main/groovy/edu/wpi/first/gradlerio/wpi/dependencies/WPIJsonDepsPlugin.groovy
+++ src/main/groovy/edu/wpi/first/gradlerio/wpi/dependencies/WPIJsonDepsPlugin.groovy
@@ -3,14 +3,21 @@ package edu.wpi.first.gradlerio.wpi.dependencies
 import com.google.gson.Gson
 import edu.wpi.first.gradlerio.wpi.WPIExtension
 import groovy.transform.CompileStatic
+import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.dsl.DependencyHandler
+import org.gradle.internal.os.OperatingSystem
+import org.gradle.nativeplatform.NativeBinarySpec
+import org.gradle.nativeplatform.TargetedNativeComponent
 
 @CompileStatic
 class WPIJsonDepsPlugin implements Plugin<Project> {
 
     static class Artifact {
-        String artifact
+        String groupId
+        String artifactId
+        String version
         String[] validClassifiers
     }
 
@@ -36,5 +43,34 @@ class WPIJsonDepsPlugin implements Plugin<Project> {
                 dependencies = new Gson().fromJson(it, JsonDependency[])
             }
         }
+
+        project.configurations.maybeCreate('wpiJsonJni')
+
+        project.extensions.add('javaVendorLibraries', { String... ignoreLibraries ->
+            def returns = []
+            if (dependencies != null) {
+                dependencies.find { (!ignoreLibraries.contains(it.name) && !ignoreLibraries.contains(it.uuid)) } each { JsonDependency dep ->
+                    dep.javaDependencies.each { Artifact art ->
+                        returns << "${art.groupId}:${art.artifactId}:${art.version}"
+                    }
+                    dep.jniDependencies.each { Artifact art ->
+                        art.validClassifiers.each {
+                            project.dependencies.add('wpiJsonJni', "${art.groupId}:${art.artifactId}:${art.version}:${it}@zip")
+                        }
+                    }
+                }
+            }
+            return returns
+        })
+
+        project.extensions.add('useCppVendorLibraries', { Object closureArg, String... ignoreLibraries ->
+            if (closureArg in TargetedNativeComponent) {
+
+            } else if (closureArg in NativeBinarySpec) {
+
+            } else {
+                throw new GradleException('Unknown type for useVendorLibraries target. You put this declaration in a weird place.')
+            }
+        })
     }
 }
