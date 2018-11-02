b768abf49708b9df5c4b0388b7477c60ae87ef2c
Thad House <ThadHouse@users.noreply.github.com>
2018-11-02 03:10:20 -0700

    Build examples during azure CI (#207)
    * Build examples during azure CI
    
    * Fix team numbers
    
    * Use upstream C++ examples
    
    * Add ldl to linker
    
    * Include a gtest main in the example
    
    * TimedKotlin
    
    * Fix kt example

---
+++ examples/kotlin/src/main/kotlin/frc/team0000/robot/Main.kt
@@ -0,0 +1,12 @@
+package frc.team0000.robot
+
+import edu.wpi.first.wpilibj.RobotBase
+
+/**
+  * Main initialization function. Do not perform any initialization here.
+  *
+  * <p>If you change your main robot class, change the parameter type.
+  */
+fun main(args : Array<String>) {
+  RobotBase.startRobot(::Robot)
+}
