557ae98a5522f47748c9a4e36df0ad9cb890338f
Jaci Brunning <jaci.brunning@gmail.com>
2018-12-19 20:55:32 +0800

    Allow loading vendor files from an arbitrary directory

--- src/main/groovy/edu/wpi/first/gradlerio/wpi/dependencies/WPIDependenciesPlugin.groovy
+++ src/main/groovy/edu/wpi/first/gradlerio/wpi/dependencies/WPIDependenciesPlugin.groovy
@@ -1,10 +1,7 @@
 package edu.wpi.first.gradlerio.wpi.dependencies
 
 import edu.wpi.first.gradlerio.wpi.WPIExtension
-import edu.wpi.first.gradlerio.wpi.WPIMavenRepo
-import groovy.json.JsonSlurper
 import groovy.transform.CompileStatic
-import jaci.gradle.log.ETLogger
 import jaci.gradle.log.ETLoggerFactory
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -30,33 +27,7 @@ class WPIDependenciesPlugin implements Plugin<Project> {
     void apply(Project project) {
         def logger = ETLoggerFactory.INSTANCE.create("WPIDeps")
         def wpi = project.extensions.getByType(WPIExtension)
-
-        loadJsonDependencies(logger, wpi)
+        wpi.deps.vendor.loadAll()
     }
 
-    private void loadJsonDependencies(ETLogger logger, WPIExtension wpi) {
-        JsonSlurper slurper = new JsonSlurper()
-
-        // Try to load dependencies JSON files
-        wpi.deps.vendor.allVendorFiles().each { File f ->
-            f.withReader {
-                try {
-                    def slurped = slurper.parse(it)
-                    wpi.deps.vendor.loadDependency(slurped)
-                } catch (e) {
-                    logger.logError("Malformed Vendor Deps File: ${f.toString()}")
-                }
-            }
-        }
-
-        // Add all URLs from dependencies
-        wpi.deps.vendor.dependencies.each { WPIVendorDepsExtension.JsonDependency dep ->
-            int i = 0
-            dep.mavenUrls.each { url ->
-                wpi.maven.vendor("${dep.uuid}_${i++}") { WPIMavenRepo repo ->
-                    repo.release = url
-                }
-            }
-        }
-    }
 }
