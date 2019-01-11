b565e4e4450fae661e644e941e8485da9b14501f
Jaci Brunning <jaci.brunning@gmail.com>
2019-01-11 08:58:20 +0800

    Download jre .ipk preemptively, fixes #294 (#295)

--- src/main/groovy/edu/wpi/first/gradlerio/wpi/dependencies/WPIDependenciesPlugin.groovy
+++ src/main/groovy/edu/wpi/first/gradlerio/wpi/dependencies/WPIDependenciesPlugin.groovy
@@ -1,11 +1,17 @@
 package edu.wpi.first.gradlerio.wpi.dependencies
 
+import edu.wpi.first.gradlerio.frc.FRCJREArtifact
 import edu.wpi.first.gradlerio.wpi.WPIExtension
 import groovy.transform.CompileStatic
+import jaci.gradle.deploy.DeployExtension
+import jaci.gradle.deploy.artifact.Artifact
 import jaci.gradle.log.ETLogger
 import jaci.gradle.log.ETLoggerFactory
+import org.gradle.api.Action
+import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.Task
 import org.gradle.api.artifacts.ResolvedArtifact
 import org.gradle.jvm.tasks.Jar
 
@@ -32,22 +38,38 @@ class WPIDependenciesPlugin implements Plugin<Project> {
         def wpi = project.extensions.getByType(WPIExtension)
         wpi.deps.vendor.loadAll()
 
-        project.tasks.withType(Jar) { Jar jarTask ->
-            jarTask.doFirst {
+        // We need to register our own task for this, since .doFirst on compileJava (or any Jar task), won't work
+        // if it's up-to-date
+        def lazyPreempt = project.tasks.register('downloadDepsPreemptively', DefaultTask, { Task t ->
+            t.doFirst {
                 // On build, download all libs that will be needed for deploy to lessen the cases where the user has to
                 // run an online deploy dry or downloadAll task.
-                downloadRoborioDepsForDeploy(project, logger)
+                downloadDepsPreemptively(project, logger)
             }
+        } as Action<Task>)
+
+        project.tasks.withType(Jar) { Jar jarTask ->
+            jarTask.dependsOn(lazyPreempt)
         }
     }
 
-    void downloadRoborioDepsForDeploy(Project project, ETLogger logger) {
-        ['nativeLib', 'nativeZip'].each {
+    void downloadDepsPreemptively(Project project, ETLogger logger) {
+        def configs = ['nativeLib', 'nativeZip']
+
+        project.extensions.getByType(DeployExtension).artifacts.each { Artifact art ->
+            if (art instanceof FRCJREArtifact) {
+                def cfgName = ((FRCJREArtifact)art).configuration()
+                logger.info("Found JRE Configuration: " + cfgName)
+                configs.add(cfgName);
+            }
+        }
+
+        configs.each {
             def cfg = project.configurations.getByName(it)
             if (cfg.canBeResolved) {
-                logger.info("Resolving RoboRIO Deps Configuration: " + cfg.getName())
+                logger.info("Resolving Deps Configuration: " + cfg.getName())
                 cfg.resolvedConfiguration.resolvedArtifacts.each { ResolvedArtifact art ->
-                    art.file
+                    logger.info(' ->' + art.file)
                 }
             } else {
                 logger.info("Can't resolve: " + cfg.getName())
