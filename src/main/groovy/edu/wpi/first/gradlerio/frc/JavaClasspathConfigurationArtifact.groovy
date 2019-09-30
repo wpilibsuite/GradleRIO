2d5081e80af591aacb10b301f4bfc50c30867fff
Thad House <thadhouse1@gmail.com>
2019-09-30 11:11:12 -0700

    Work on allowing separate jars to be deployed for Java apps

---
+++ src/main/groovy/edu/wpi/first/gradlerio/frc/JavaClasspathConfigurationArtifact.groovy
@@ -0,0 +1,70 @@
+package edu.wpi.first.gradlerio.frc
+
+import groovy.transform.CompileStatic
+import javax.inject.Inject
+import jaci.gradle.deploy.artifact.FileCollectionArtifact
+import jaci.gradle.deploy.context.DeployContext
+import jaci.gradle.ActionWrapper
+import org.gradle.api.Action
+import jaci.gradle.Resolver
+import jaci.gradle.deploy.cache.CacheMethod
+import org.gradle.api.Project
+import org.gradle.api.artifacts.Configuration
+import org.gradle.api.file.FileCollection
+import org.gradle.api.tasks.util.PatternFilterable
+import jaci.gradle.deploy.artifact.AbstractArtifact
+import org.gradle.api.provider.Property
+import jaci.gradle.PathUtils
+
+import java.util.concurrent.Callable
+
+@CompileStatic
+class JavaClasspathConfigurationArtifact extends AbstractArtifact implements Callable<FileCollection> {
+
+    Configuration configuration
+    final Property<FileCollection> files
+    private FileCollection resolvedFiles
+
+    @Inject
+    JavaClasspathConfigurationArtifact(String name, Project project) {
+        super(name, project)
+        files = project.objects.property(FileCollection)
+        directory = PathUtils.combine(FRCPlugin.LIB_DEPLOY_DIR, 'java')
+        onlyIf = { DeployContext ctx ->
+            files.isPresent() && !files.get().empty && !files.get().files.empty
+        }
+
+        files.set(project.files(this as Callable<FileCollection>))
+    }
+
+    Object cache = "md5sum"
+    Resolver<CacheMethod> cacheResolver
+
+    @Override
+    void deploy(DeployContext context) {
+        if (files.isPresent()) {
+            Map<String, File> filesToWrite = [:]
+            files.get().files.each {
+                filesToWrite.put(it.name.replaceFirst(~/\.[^\.] $/, ''), it)
+            }
+            context.put(filesToWrite, cacheResolver?.resolve(cache))
+        } else {
+            context.logger?.log("No file(s) provided for ${toString()}")
+        }
+    }
+
+    List<String> getFileNames() {
+        return files.get().files.collect { it.name.replaceFirst(~/\.[^\.] $/, '') }
+    }
+
+    @Override
+    FileCollection call() {
+        if (resolvedFiles != null) {
+            return resolvedFiles
+        }
+        def conf = configuration.resolvedConfiguration
+        resolvedFiles = project.files(conf.files)
+
+        return resolvedFiles
+    }
+}
