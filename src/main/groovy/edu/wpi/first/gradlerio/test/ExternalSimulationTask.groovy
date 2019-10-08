13f0b37b2d016ff9d4a0c22e0743a9fbb477c492
Thad House <thadhouse1@gmail.com>
2019-10-08 13:22:53 -0700

    Get Sim GUI working
    Also fixes a few bugs causing configurations not to update. At some point things should be made incremental, but this works for now

--- src/main/groovy/edu/wpi/first/gradlerio/test/ExternalSimulationTask.groovy
+++ src/main/groovy/edu/wpi/first/gradlerio/test/ExternalSimulationTask.groovy
@@ -2,4 +2,9 @@ package edu.wpi.first.gradlerio.test
 
 import org.gradle.api.DefaultTask
 
-class ExternalSimulationTask extends DefaultTask { }
+class ExternalSimulationTask extends DefaultTask {
+
+    ExternalSimulationTask() {
+        outputs.upToDateWhen { false }
+    }
+ }
