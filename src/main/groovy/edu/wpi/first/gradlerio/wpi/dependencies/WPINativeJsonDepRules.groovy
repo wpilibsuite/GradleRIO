39a3cd9083903ab485c1946e9cfea19896cd14ae
Thad House <ThadHouse@users.noreply.github.com>
2019-11-23 11:34:30 -0800

    Fix vendor library linking to incorrect version on deploy (#380)

--- src/main/groovy/edu/wpi/first/gradlerio/wpi/dependencies/WPINativeJsonDepRules.groovy
+++ src/main/groovy/edu/wpi/first/gradlerio/wpi/dependencies/WPINativeJsonDepRules.groovy
@@ -86,7 +86,7 @@ class WPINativeJsonDepRules extends RuleSource {
                                 } else {
                                     lib.staticMatchers.add("**/*${cpp.libName}*.a".toString())
                                 }
-                                lib.maven = "$mavenbase:${platform}${linkSuff}debug@zip"
+                                lib.maven = "$mavenbase:${platform}${linkSuff}$buildKind@zip"
                                 // It can't be 'config' otherwise missing libs break even if not used!
                                 lib.configuration = "${binaryConfig}_${platform}".toString()
                             } as Action<NativeLib>)
