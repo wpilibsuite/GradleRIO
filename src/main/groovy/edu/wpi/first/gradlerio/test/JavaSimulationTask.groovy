16bcbca89afa96dee6f2630dd1e82cd1ac4d32dd
Thad House <thadhouse1@gmail.com>
2020-01-29 09:54:41 -0800

    Make Java Simulation task use wpilib JDK, message about potential missing runtime

--- src/main/groovy/edu/wpi/first/gradlerio/test/JavaSimulationTask.groovy
+++ src/main/groovy/edu/wpi/first/gradlerio/test/JavaSimulationTask.groovy
@@ -1,5 +1,6 @@
 package edu.wpi.first.gradlerio.test
 
+import edu.wpi.first.gradlerio.wpi.WPIExtension
 import edu.wpi.first.gradlerio.ExternalLaunchTask
 import edu.wpi.first.gradlerio.test.JavaTestPlugin
 import edu.wpi.first.gradlerio.test.TestPlugin
@@ -9,13 +10,26 @@ import org.gradle.api.tasks.TaskAction
 import org.gradle.internal.os.OperatingSystem
 import org.gradle.internal.jvm.Jvm
 import org.gradle.jvm.tasks.Jar
+import java.nio.file.Paths
 
 @CompileStatic
 class JavaSimulationTask extends ExternalLaunchTask {
     @TaskAction
     void run() {
         def ldpath = JavaTestPlugin.jniExtractionDir(project).absolutePath
-        def java = Jvm.current().getExecutable("java").absolutePath
+
+        def javaFile = Jvm.current().getExecutable("java")
+        def java = javaFile.absolutePath
+        def javaFileName = javaFile.name
+
+        // Search for WPILib JDK first
+        def frcHome = project.extensions.getByType(WPIExtension).frcHome
+        //C:\Users\Public\wpilib\2020\jdk\bin
+        def jdkPath = Paths.get(frcHome, "jdk", "bin").toFile()
+        if (jdkPath.exists()) {
+            java = (new File(jdkPath, javaFileName)).absolutePath.toString()
+        }
+
         environment = TestPlugin.getSimLaunchEnv(project, ldpath)
         for (Jar jar : taskDependencies.getDependencies(this).findAll { it instanceof Jar } as Set<Jar>) {
             def manifestAttributes = jar.manifest.attributes
@@ -27,6 +41,10 @@ class JavaSimulationTask extends ExternalLaunchTask {
             if (OperatingSystem.current().isMacOsX()) {
                 launch("\"$java\"", "-Djava.library.path=\"$ldpath\"", "-XstartOnFirstThread", "-jar", "\"${jar.archivePath.toString()}\"")
             } else {
+                if (OperatingSystem.current().isWindows()) {
+                    println "If you receive errors loading the JNI dependencies, make sure you have the latest Visual Studio C++ Redstributable installed."
+                    println "That can be found at https://support.microsoft.com/en-us/help/2977003/the-latest-supported-visual-c-downloads"
+                }
                 launch("\"$java\"", "-Djava.library.path=\"$ldpath\"", "-jar", "\"${jar.archivePath.toString()}\"")
             }
             return
