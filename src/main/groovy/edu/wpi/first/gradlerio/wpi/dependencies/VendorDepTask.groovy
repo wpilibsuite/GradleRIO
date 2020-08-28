426e8e21134c1c765ca5d7dfcc56489d459f78d4
Starlight220 <yotamshlomi@gmail.com>
2020-08-28 09:48:54 +0300

    expand error message

--- src/main/groovy/edu/wpi/first/gradlerio/wpi/dependencies/VendorDepTask.groovy
+++ src/main/groovy/edu/wpi/first/gradlerio/wpi/dependencies/VendorDepTask.groovy
@@ -4,6 +4,7 @@ import de.undercouch.gradle.tasks.download.DownloadAction
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.options.Option
+
 /**
  * A task type for downloading vendordep JSON files from the vendor URL.
  */
@@ -22,12 +23,14 @@ class VendorDepTask extends DefaultTask{
      * @return the name of the JSON file, with the `.json` suffix
      */
     private static String findFileName(String url) {
-        if(url == null) {
-            throw new IllegalArgumentException("No vendor JSON URL was entered.")
+        if (url == null) {
+            throw new IllegalArgumentException(
+                    "No vendor JSON URL was entered. Try the following:\n\tgradlew vendordep --url <insert_url_here>")
         }
         int lastUrlSeparator = url.lastIndexOf('/')
-        if(lastUrlSeparator == -1) {
-            throw new IllegalArgumentException("No vendor JSON URL was entered.")
+        if (lastUrlSeparator == -1) {
+            throw new IllegalArgumentException(
+                    "No vendor JSON URL was entered. Try the following:\n\tgradlew vendordep --url <insert_url_here>")
         }
         String name = url.substring(lastUrlSeparator + 1)
         return "vendordeps/${name}"
@@ -37,7 +40,7 @@ class VendorDepTask extends DefaultTask{
      * Installs the JSON file
      */
     @TaskAction
-    public void install() throws IOException {
+    void install() throws IOException {
         da.src(url)
         da.dest(findFileName(url))
         da.execute()
