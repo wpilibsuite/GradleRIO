dde33f253548534f6f57719725c03db59b2b3b11
Tyler Veness <calcmogul@gmail.com>
2020-11-12 18:19:52 -0800

    Replace ScriptBase.sh with ScriptBase.py

--- src/main/resources/ScriptBase.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-SCRIPTFULLNAME=`basename "$0"`
-SCRIPTNAME="${SCRIPTFULLNAME%.*}"
-
-JARPATH="$SCRIPTPATH/$SCRIPTNAME.jar"
-
-java -jar $JARPATH >/dev/null 2>&1 &
