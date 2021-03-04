d6c6118bd49f5fd4163b9a897e5a8f0554b216a5
Thad House <thadhouse1@gmail.com>
2021-03-04 08:24:12 -0800

    Get a bit more java working

--- src/main/java/edu/wpi/first/gradlerio/wpi/java/ExtractNativeJavaArtifacts.java
+++ src/main/java/edu/wpi/first/gradlerio/wpi/java/ExtractNativeJavaArtifacts.java
@@ -1,33 +1,23 @@
 package edu.wpi.first.gradlerio.wpi.java;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.inject.Inject;
 
 import org.gradle.api.DefaultTask;
-import org.gradle.api.artifacts.ArtifactView;
-import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.file.ConfigurableFileCollection;
 import org.gradle.api.file.DirectoryProperty;
 import org.gradle.api.file.FileSystemOperations;
 import org.gradle.api.tasks.InputFiles;
 import org.gradle.api.tasks.OutputDirectory;
 import org.gradle.api.tasks.TaskAction;
-import org.gradle.api.tasks.util.PatternFilterable;
-import org.gradle.api.tasks.util.PatternSet;
 import org.gradle.workers.WorkAction;
 import org.gradle.workers.WorkParameters;
 import org.gradle.workers.WorkQueue;
 import org.gradle.workers.WorkerExecutor;
 
-import edu.wpi.first.gradlerio.wpi.WPIPlugin;
-
 public abstract class ExtractNativeJavaArtifacts extends DefaultTask {
 
     public static interface ExtractParameters extends WorkParameters {
-        List<File> getFiles();
+        ConfigurableFileCollection getFiles();
         DirectoryProperty getDestinationDirectory();
     }
 
@@ -45,14 +35,8 @@ public abstract class ExtractNativeJavaArtifacts extends DefaultTask {
         }
     }
 
-    private final List<Configuration> configurations = new ArrayList<>();
-
-    // This needs to take a configuration
-
     @InputFiles
-    public List<Configuration> getConfigurations() {
-
-    }
+    public abstract ConfigurableFileCollection getFiles();
 
     @OutputDirectory
     public abstract DirectoryProperty getDestinationDirectory();
@@ -64,24 +48,8 @@ public abstract class ExtractNativeJavaArtifacts extends DefaultTask {
     public void execute() {
         WorkQueue workQueue = getWorkerExecuter().noIsolation();
 
-        PatternFilterable filterable = new PatternSet();
-        filterable.include("**/*.so", "**/*.dylib", "**/*.pdb", "**/*.dll");
-
-        List<File> files = new ArrayList<>();
-
-        for (Configuration configuration : configurations) {
-            ArtifactView view = configuration.getIncoming().artifactView(viewConfiguration -> {
-                viewConfiguration.attributes(attributeContainer -> {
-                    attributeContainer.attribute(WPIPlugin.NATIVE_ARTIFACT_FORMAT,
-                    WPIPlugin.NATIVE_ARTIFACT_DIRECTORY_TYPE);
-                });
-            });
-
-            files.addAll(view.getFiles().getAsFileTree().matching(filterable).getFiles());
-        }
-
         workQueue.submit(ExtractFiles.class, parameters -> {
-            parameters.getFiles().setFrom();
+            parameters.getFiles().setFrom(getFiles());
             parameters.getDestinationDirectory().set(getDestinationDirectory());
         });
     }
--- src/main/java/edu/wpi/first/gradlerio/wpi/java/WPIJavaExtension.java
+++ src/main/java/edu/wpi/first/gradlerio/wpi/java/WPIJavaExtension.java
@@ -1,11 +1,18 @@
 package edu.wpi.first.gradlerio.wpi.java;
 
+import java.io.File;
+import java.util.Set;
+import java.util.concurrent.Callable;
+
 import javax.inject.Inject;
 
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.ArtifactView;
 import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.file.FileCollection;
 import org.gradle.api.tasks.TaskProvider;
+import org.gradle.api.tasks.util.PatternFilterable;
+import org.gradle.api.tasks.util.PatternSet;
 
 import edu.wpi.first.gradlerio.wpi.WPIPlugin;
 import edu.wpi.first.gradlerio.wpi.WPIVersionsExtension;
@@ -47,6 +54,18 @@ public class WPIJavaExtension {
         return releaseNativeConfiguration;
     }
 
+    private final FileCollection debugFileCollection;
+    private final FileCollection releaseFileCollection;
+
+    public FileCollection getDebugFileCollection() {
+        return debugFileCollection;
+    }
+
+    public FileCollection getReleaseFileCollection() {
+        return releaseFileCollection;
+    }
+
+
     @Inject
     public WPIJavaExtension(Project project, SimulationExtension sim, WPIVersionsExtension versions, WPIVendorDepsExtension vendorDeps) {
         deps = project.getObjects().newInstance(WPIJavaDepsExtension.class, versions);
@@ -55,12 +74,39 @@ public class WPIJavaExtension {
         debugNativeConfiguration = project.getConfigurations().create("nativeDebug");
         releaseNativeConfiguration = project.getConfigurations().create("nativeRelease");
 
+        PatternFilterable filterable = new PatternSet();
+        filterable.include("**/*.so", "**/*.dylib", "**/*.pdb", "**/*.dll");
+
+        ArtifactView debugView = debugNativeConfiguration.getIncoming().artifactView(viewConfiguration -> {
+            viewConfiguration.attributes(attributeContainer -> {
+                attributeContainer.attribute(WPIPlugin.NATIVE_ARTIFACT_FORMAT,
+                WPIPlugin.NATIVE_ARTIFACT_DIRECTORY_TYPE);
+            });
+        });
+
+        ArtifactView releaseView = releaseNativeConfiguration.getIncoming().artifactView(viewConfiguration -> {
+            viewConfiguration.attributes(attributeContainer -> {
+                attributeContainer.attribute(WPIPlugin.NATIVE_ARTIFACT_FORMAT,
+                WPIPlugin.NATIVE_ARTIFACT_DIRECTORY_TYPE);
+            });
+        });
+
+        Callable<Set<File>> debugCallable = () -> debugView.getFiles().getAsFileTree().matching(filterable).getFiles();
+        Callable<Set<File>> releaseCallable = () -> releaseView.getFiles().getAsFileTree().matching(filterable).getFiles();
+
+        debugFileCollection = project.files(debugCallable);
+        releaseFileCollection = project.files(releaseCallable);
+
         extractNativeDebugArtifacts = project.getTasks().register("extractDebugNative", ExtractNativeJavaArtifacts.class, extract -> {
             extract.getDestinationDirectory().set(project.getLayout().getBuildDirectory().dir("jni/debug"));
+            extract.getFiles().from(sim.getDebugFileCollection());
+            extract.getFiles().from(debugFileCollection);
         });
 
         extractNativeReleaseArtifacts = project.getTasks().register("extractReleaseNative", ExtractNativeJavaArtifacts.class, extract -> {
             extract.getDestinationDirectory().set(project.getLayout().getBuildDirectory().dir("jni/release"));
+            extract.getFiles().from(sim.getReleaseFileCollection());
+            extract.getFiles().from(releaseFileCollection);
         });
     }
 }
