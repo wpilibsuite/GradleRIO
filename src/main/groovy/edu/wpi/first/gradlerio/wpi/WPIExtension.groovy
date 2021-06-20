8d1b3f07d0a118e26a1f36724f5090120a05eafb
Peter Johnson <johnson.peter@gmail.com>
2021-06-19 23:19:09 -0700

    Update for 2022.0.0-alpha-2
    Revert tools to 2021.3.1
    Update examples
    Bump frcYear to 2022

--- src/main/groovy/edu/wpi/first/gradlerio/wpi/WPIExtension.groovy
+++ src/main/groovy/edu/wpi/first/gradlerio/wpi/WPIExtension.groovy
@@ -16,13 +16,13 @@ import javax.inject.Inject
 @CompileStatic
 class WPIExtension {
     // WPILib (first.wpi.edu/FRC/roborio/maven) libs
-    String wpilibVersion = "2022.0.0"
+    String wpilibVersion = "2022.0.0-alpha-2"
     String niLibrariesVersion = "2020.10.1"
     String opencvVersion = "4.5.2-1"
     String imguiVersion = "1.82-1"
     String ejmlVersion = "0.38"
     String jacksonVersion = "2.10.0"
-    String wpimathVersion = "2022.0.0"
+    String wpimathVersion = "2022.0.0-alpha-2"
     static final String[] validImageVersions = ['2020_v10', '2021_v1', '2021_v2', '2021_v3.*']
 
     String googleTestVersion = "1.9.0-5-437e100-1"
@@ -30,18 +30,18 @@ class WPIExtension {
     String jreArtifactLocation = "edu.wpi.first.jdk:roborio-2021:11.0.9u11-1"
 
     // WPILib (first.wpi.edu/FRC/roborio/maven) Utilities
-    String smartDashboardVersion = "2022.0.0"
-    String shuffleboardVersion = "2022.0.0"
-    String outlineViewerVersion = "2022.0.0"
-    String robotBuilderVersion = "2022.0.0"
-    String robotBuilderOldVersion = "2022.0.0"
-    String pathWeaverVersion = "2022.0.0"
-    String glassVersion = "2022.0.0"
+    String smartDashboardVersion = "2021.3.1"
+    String shuffleboardVersion = "2021.3.1"
+    String outlineViewerVersion = "2022.0.0-alpha-2"
+    String robotBuilderVersion = "2021.3.1"
+    String robotBuilderOldVersion = "2021.3.1"
+    String pathWeaverVersion = "2021.3.1"
+    String glassVersion = "2022.0.0-alpha-2"
 
     WPIMavenExtension maven
     WPIDepsExtension deps
 
-    String frcYear = '2021'
+    String frcYear = '2022'
 
     NativePlatforms platforms;
 
