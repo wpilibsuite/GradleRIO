f34848ad4572f32cfa4321cb0afc16bac5a4491b
Thad House <thadhouse1@gmail.com>
2021-10-06 16:52:32 -0700

    Skip transitive dependencies of vendor deps

--- src/main/java/edu/wpi/first/gradlerio/wpi/dependencies/WPIVendorDepsExtension.java
+++ src/main/java/edu/wpi/first/gradlerio/wpi/dependencies/WPIVendorDepsExtension.java
@@ -14,6 +14,7 @@ import javax.inject.Inject;
 import com.google.gson.Gson;
 
 import org.gradle.api.Project;
+import org.gradle.api.artifacts.Dependency;
 import org.gradle.api.provider.ProviderFactory;
 
 import edu.wpi.first.deployutils.log.ETLogger;
