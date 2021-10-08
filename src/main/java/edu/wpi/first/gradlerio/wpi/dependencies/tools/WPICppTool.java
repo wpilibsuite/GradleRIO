2926965c7c7fee926f01994e512a70bf114561c2
Thad House <ThadHouse@users.noreply.github.com>
2021-10-07 18:42:29 -0700

    Register C++ tools as a dependency (#527)

--- src/main/java/edu/wpi/first/gradlerio/wpi/dependencies/tools/WPICppTool.java
+++ src/main/java/edu/wpi/first/gradlerio/wpi/dependencies/tools/WPICppTool.java
@@ -2,10 +2,11 @@ package edu.wpi.first.gradlerio.wpi.dependencies.tools;
 
 import org.gradle.api.Named;
 import org.gradle.api.Project;
+import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.provider.Provider;
 import org.gradle.api.tasks.TaskProvider;
 
-// import edu.wpi.first.gradlerio.wpi.WPIExtension;
+import edu.wpi.first.gradlerio.wpi.WPIExtension;
 
 public class WPICppTool implements Named {
     //TaskProvider<CppToolInstallTask> toolInstallTask
@@ -16,15 +17,15 @@ public class WPICppTool implements Named {
     private final Provider<String> version;
 
     public WPICppTool(Project project, String name, Provider<String> version, String artifactId) {
-        // Configuration config = project.getConfigurations().getByName("wpiCppTools");
-        // String toolsClassifier = project.getExtensions().getByType(WPIExtension.class).getCppToolsClassifier();
-        // Provider<String> fullId = project.getProviders().provider(() -> {
-        //     String id = artifactId;
-        //     id += ":" + version.get();
-        //     id += ":" + toolsClassifier + "@zip";
-        //     return id;
-        // });
-        //Dependency dependency = project.getDependencies().add("wpiCppTools", artifactId);
+        Configuration config = project.getConfigurations().getByName("wpiCppTools");
+        String toolsClassifier = project.getExtensions().getByType(WPIExtension.class).getCppToolsClassifier();
+        Provider<String> fullId = project.getProviders().provider(() -> {
+            String id = artifactId;
+            id += ":" + version.get();
+            id += ":" + toolsClassifier + "@zip";
+            return id;
+        });
+        project.getDependencies().add("wpiCppTools", fullId);
         //toolInstallTask = project.tasks.register("${name}Install".toString(), CppToolInstallTask, name, config, dependency)
         toolRunTask = project.getTasks().register(name, CppToolRunTask.class, name);
         this.name = name;
