4a441041c4b5d3fa612dd04feb9f3dff9cff0cab
Thad House <thadhouse1@gmail.com>
2021-11-26 21:05:26 -0800

    Switch back to vbs, just add arguemnts

--- src/main/java/edu/wpi/first/gradlerio/wpi/dependencies/tools/CppToolRunTask.java
+++ src/main/java/edu/wpi/first/gradlerio/wpi/dependencies/tools/CppToolRunTask.java
@@ -31,18 +31,11 @@ public class CppToolRunTask extends DefaultTask implements SingletonTask {
 
     @TaskAction
     public void runTool() {
-        ProcessBuilder builder = getProcessBuilder();
-        try {
-            Process proc = builder.start();
-            // Wait 3 seconds
-            Thread.sleep(3000);
-            if (!proc.isAlive()) {
-                String stdOut = IOGroovyMethods.getText(proc.getInputStream());
-                String stdErr = IOGroovyMethods.getText(proc.getErrorStream());
-                throw new ToolRunException(stdOut, stdErr);
-            }
-        } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
+        boolean isWindows = OperatingSystem.current().isWindows();
+        if (isWindows) {
+            runToolWindows();
+        } else {
+            runToolUnix();
         }
     }
 
@@ -50,18 +43,29 @@ public class CppToolRunTask extends DefaultTask implements SingletonTask {
         return new File(getProject().getProjectDir(), "." + toolNameLower).getAbsolutePath();
     }
 
-    private ProcessBuilder getProcessBuilder() {
-        String toolNameLower = toolName.toLowerCase();
-        if (OperatingSystem.current().isWindows()) {
-            String outputFile = new File(ToolInstallTask.getToolsFolder(), toolName + ".exe").getAbsolutePath();
-            return new ProcessBuilder(outputFile, getArgumentPath(toolNameLower));
-        } else if (OperatingSystem.current().isMacOsX()) {
-            String outputFile = new File(ToolInstallTask.getToolsFolder(), toolName + ".app").getAbsolutePath();
-            return new ProcessBuilder("open", outputFile, getArgumentPath(toolNameLower));
-        } else {
-            String outputFile = new File(ToolInstallTask.getToolsFolder(), toolNameLower).getAbsolutePath();
-            return new ProcessBuilder(outputFile, getArgumentPath(toolNameLower));
+    private void runToolWindows() {
+        File outputFile = new File(ToolInstallTask.getToolsFolder(), toolName + ".vbs");
+        ProcessBuilder builder = new ProcessBuilder("wscript.exe", outputFile.getAbsolutePath(), "silent", getArgumentPath(toolName.toLowerCase()));
+        try {
+            Process proc = builder.start();
+            int result = proc.waitFor();
+            if (result != 0) {
+                String stdOut = IOGroovyMethods.getText(proc.getInputStream());
+                String stdErr = IOGroovyMethods.getText(proc.getErrorStream());
+                throw new ToolRunException(stdOut, stdErr);
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
         }
+
+    }
+
+    private void runToolUnix() {
+        File outputFile = new File(ToolInstallTask.getToolsFolder(), toolName + ".py");
+        getProject().exec(spec -> {
+            spec.setExecutable(outputFile.getAbsolutePath());
+            spec.args(getArgumentPath(toolName.toLowerCase()));
+        });
     }
 
     @Override
