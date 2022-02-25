4763f41cc1f419369525bd909f6b8615dff4c3da
Thad House <thadhouse1@gmail.com>
2022-02-24 18:22:00 -0800

    Add version check logic for FRC, downgrade jre

--- src/main/java/edu/wpi/first/gradlerio/deploy/roborio/FRCJREArtifact.java
+++ src/main/java/edu/wpi/first/gradlerio/deploy/roborio/FRCJREArtifact.java
@@ -4,6 +4,7 @@ import javax.inject.Inject;
 
 import org.gradle.api.Project;
 
+import edu.wpi.first.deployutils.deploy.CommandDeployResult;
 import edu.wpi.first.deployutils.deploy.artifact.MavenArtifact;
 import edu.wpi.first.deployutils.deploy.context.DeployContext;
 import edu.wpi.first.gradlerio.deploy.DeployStage;
@@ -17,17 +18,29 @@ public class FRCJREArtifact extends MavenArtifact {
         return configName;
     }
 
+    public boolean isCheckJreVersion() {
+        return checkJreVersion;
+    }
+
+    public void setCheckJreVersion(boolean checkJreVersion) {
+        this.checkJreVersion = checkJreVersion;
+    }
+
+    private boolean checkJreVersion = true;
+
+
     @Inject
     public FRCJREArtifact(String name, StagedDeployTarget target) {
         super(name, target);
         String configName = name + "frcjre";
         this.configName = configName;
         Project project = target.getProject();
+        WPIExtension wpiExt = project.getExtensions().getByType(WPIExtension.class);
         getConfiguration().set(project.getConfigurations().create(configName));
-        getDependency().set(project.getDependencies().add(configName, project.getExtensions().getByType(WPIExtension.class).getJreArtifactLocation()));
+        getDependency().set(project.getDependencies().add(configName, wpiExt.getJreArtifactLocation()));
 
         setOnlyIf(ctx -> {
-            return jreMissing(ctx) || project.hasProperty("force-redeploy-jre");
+            return jreMissing(ctx) || jreOutOfDate(ctx) || project.hasProperty("force-redeploy-jre");
         });
 
         getDirectory().set("/tmp");
@@ -35,7 +48,7 @@ public class FRCJREArtifact extends MavenArtifact {
 
         getPostdeploy().add(ctx -> {
             ctx.getLogger().log("Installing JRE...");
-            ctx.execute("opkg remove frc2022-openjdk*; opkg install /tmp/frcjre.ipk; rm /tmp/frcjre.ipk");
+            ctx.execute("opkg remove frc*-openjdk*; opkg install /tmp/frcjre.ipk; rm /tmp/frcjre.ipk");
             ctx.getLogger().log("JRE Deployed!");
         });
 
@@ -46,5 +59,21 @@ public class FRCJREArtifact extends MavenArtifact {
         return ctx.execute("if [[ -f \"/usr/local/frc/JRE/bin/java\" ]]; then echo OK; else echo MISSING; fi").getResult().contains("MISSING");
     }
 
-
+    private boolean jreOutOfDate(DeployContext ctx) {
+        if (!checkJreVersion) {
+            return false;
+        }
+        String version = getDependency().get().getVersion();
+        CommandDeployResult cmdResult = ctx.execute("opkg list-installed | grep openjdk");
+        if (cmdResult.getExitCode() != 0) {
+            ctx.getLogger().log("JRE not found");
+            return false;
+        }
+        String result = cmdResult.getResult().trim();
+        ctx.getLogger().log("Searching for JRE " + version);
+        ctx.getLogger().log("Found JRE " + result);
+        boolean matches = result.contains(version);
+        ctx.getLogger().log(matches ? "JRE Is Correct Version" : "JRE is mismatched. Reinstalling");
+        return !matches;
+    }
 }
