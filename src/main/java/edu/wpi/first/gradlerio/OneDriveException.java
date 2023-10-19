bc8d841a89c00c1c2f83c7e142e4ee0a485f07f1
Isaac Turner <spacey_sooty@outlook.com>
2023-10-19 10:20:02 +0800

    remove another unnecessary import

--- src/main/java/edu/wpi/first/gradlerio/OneDriveException.java
+++ src/main/java/edu/wpi/first/gradlerio/OneDriveException.java
@@ -1,7 +1,5 @@
 package edu.wpi.first.gradlerio;
 
-import java.lang.Exception;
-
 public class OneDriveException extends java.lang.RuntimeException {
   public OneDriveException() {
     super(String.format("Error cannot create project inside OneDrive. Project Directory = %S", System.getProperty("user.dir")));
