836419ab8da6251ea19cb22aba4f1c8068dffc34
Isaac Turner <spacey_sooty@outlook.com>
2023-12-22 20:41:12 +0000

    Bump quickbuf to 1.3.3 (#707)

--- src/main/java/edu/wpi/first/gradlerio/wpi/WPIVersionsExtension.java
+++ src/main/java/edu/wpi/first/gradlerio/wpi/WPIVersionsExtension.java
@@ -12,7 +12,7 @@ public abstract class WPIVersionsExtension {
     private static final String imguiVersion = "1.89.9-1";
     private static final String ejmlVersion = "0.43.1";
     private static final String jacksonVersion = "2.15.2";
-    private static final String quickbufVersion = "1.3.2";
+    private static final String quickbufVersion = "1.3.3";
     private static final String wpimathVersion = "2024.1.1-beta-4";
     private static final String googleTestVersion = "1.14.0-1";
 
