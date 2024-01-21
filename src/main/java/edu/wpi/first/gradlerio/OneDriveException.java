f37a5572ed0be040cccbe9e62c7e3e86cf40639b
Isaac Turner <spacey_sooty@outlook.com>
2024-01-22 03:18:53 +0800

    Reword OneDrive error to be more useful (#716)

--- src/main/java/edu/wpi/first/gradlerio/OneDriveException.java
+++ src/main/java/edu/wpi/first/gradlerio/OneDriveException.java
@@ -6,5 +6,6 @@ public class OneDriveException extends java.lang.RuntimeException {
   public OneDriveException(Project project) {
     super(String.format("Error cannot create project inside OneDrive. Project Directory = %S", project.getRootDir().toString()));
     System.out.println(String.format("Error cannot create project inside OneDrive. Project Directory = %S", project.getRootDir().toString()));
+    System.out.println("To fix this error move your project out of the OneDrive folder.");
   }
 }
