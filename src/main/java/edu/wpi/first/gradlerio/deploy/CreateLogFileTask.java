a2c42c792bcbdff33713f73a8472b18b4dfb792f
Isaac Turner <spacey_sooty@outlook.com>
2024-03-28 14:15:29 +0800

    should be working now

--- src/main/java/edu/wpi/first/gradlerio/deploy/CreateLogFileTask.java
+++ src/main/java/edu/wpi/first/gradlerio/deploy/CreateLogFileTask.java
@@ -5,9 +5,12 @@ import com.google.gson.Gson;
 
 import org.ajoberstar.grgit.Grgit;
 
+import javax.inject.Inject;
+
+import org.codehaus.groovy.runtime.ResourceGroovyMethods;
+
 import org.gradle.api.DefaultTask;
 import org.gradle.api.GradleException;
-import org.gradle.api.file.RegularFileProperty;
 import org.gradle.api.tasks.TaskAction;
 import org.gradle.api.tasks.OutputFile;
 
@@ -28,12 +31,15 @@ public class CreateLogFileTask extends DefaultTask {
             "gitDesc",
     };
     private File deployFile;
+    private String json;
 
-    @TaskAction
-    public void execute() {
-        Gson builder = new GsonBuilder().create();
-        Grgit grgit;
+    @Inject
+    public CreateLogFileTask() {
         HashMap<String, String> data = new HashMap<String, String>();
+        GsonBuilder builder = new GsonBuilder();
+        builder.setPrettyPrinting();
+        Gson jsongen = builder.create();
+        Grgit grgit;
 
         try {
             grgit = Grgit.open();
@@ -59,7 +65,8 @@ public class CreateLogFileTask extends DefaultTask {
             } catch (Exception e) {
                 throw new GradleException("Couldn't get git description", e);
             }
-        } catch (Exception e) {}
+        } catch (Exception e) {
+        }
 
         try {
             data.put(DEPLOY_ITEMS[0], Runtime.getRuntime().exec("hostname").getOutputStream().toString().strip());
@@ -71,12 +78,21 @@ public class CreateLogFileTask extends DefaultTask {
         data.put(DEPLOY_ITEMS[2], LocalDateTime.now().toString());
         data.put(DEPLOY_ITEMS[3], System.getProperty("user.dir"));
 
-        deployFile = new File(builder.toJson(data));
+        json = jsongen.toJson(data);
+    }
+
+    @TaskAction
+    public void execute() throws IOException {
+        deployFile.getParentFile().mkdirs();
+        ResourceGroovyMethods.setText(deployFile, json);
+    }
+
+    public void setDeployFile(String path) {
+      deployFile = new File(path);
     }
 
     @OutputFile
-    public RegularFileProperty getDeployFile() {
-        // TODO! figure out how to do this
+    public File getDeployFile() {
         return deployFile;
     };
 }
