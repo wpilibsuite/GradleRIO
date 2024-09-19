wpilibsuite/GradleRIO

commit be9df73eaeb7630c6f4c4f7e35f904dd8996b9a9

Fix build artifact upload (#745)
Files/directories starting with . are now considered "hidden" and won't be uploaded by default. Uploading only the repository makes it so .m2 isn't in the recursive glob, and ensures settings.xml doesn't get uploaded (good practice since it can potentially contain repository credentials).

.github/workflows/ci.yml
@@ -20,7 +20,7 @@ jobs:
     - uses: actions/upload-artifact@v4
       with:
         name: Maven
-        path: ~/.m2/
+        path: ~/.m2/repository/
 
   test_examples:
     needs: build

commit dceeba8f7917068c35cf9675226c55fba9478aa6

Update for 2025.0.0-alpha-2 (#749)
* Update frcYear to 2025

* Update for 2025.0.0-alpha-2

* Fix test extraction directory (broken by #745)

.github/workflows/ci.yml
@@ -37,7 +37,7 @@ jobs:
     - uses: actions/download-artifact@v4
       with:
         name: Maven
-        path: ~/.m2/
+        path: ~/.m2/repository/
 
     - name: Setup RoboRIO Toolchain
       if: ${{ (matrix.language == 'cpp') || (matrix.language == 'asm') }}

gradle.properties
@@ -1,1 +1,1 @@
-version = 2024.3.2
+version = 2025.0.0-alpha-2

src/main/java/edu/wpi/first/gradlerio/wpi/WPIExtension.java
@@ -58,7 +58,7 @@ public class WPIExtension {
         platforms = new NativePlatforms();
 
         frcYear = factory.property(String.class);
-        frcYear.convention("2024");
+        frcYear.convention("2025");
 
         frcHome = factory.directoryProperty().fileProvider(project.provider(WPIExtension::computeHomeRoot))
                 .dir(frcYear);

src/main/java/edu/wpi/first/gradlerio/wpi/WPIVersionsExtension.java
@@ -6,25 +6,25 @@ import org.gradle.api.provider.Property;
 
 public abstract class WPIVersionsExtension {
 
-    private static final String wpilibVersion = "2024.3.2";
+    private static final String wpilibVersion = "2025.0.0-alpha-2";
     private static final String niLibrariesVersion = "2024.2.1";
-    private static final String opencvVersion = "4.8.0-2";
+    private static final String opencvVersion = "4.8.0-4";
     private static final String imguiVersion = "1.89.9-1";
     private static final String ejmlVersion = "0.43.1";
     private static final String jacksonVersion = "2.15.2";
     private static final String quickbufVersion = "1.3.3";
-    private static final String wpimathVersion = "2024.3.2";
+    private static final String wpimathVersion = "2025.0.0-alpha-2";
     private static final String googleTestVersion = "1.14.0-1";
 
-    private static final String smartDashboardVersion = "2024.3.2";
-    private static final String shuffleboardVersion = "2024.3.2";
-    private static final String outlineViewerVersion = "2024.3.2";
-    private static final String robotBuilderVersion = "2024.3.2";
-    private static final String pathWeaverVersion = "2024.3.2";
-    private static final String glassVersion = "2024.3.2";
-    private static final String sysIdVersion = "2024.3.2";
-    private static final String roboRIOTeamNumberSetterVersion = "2024.3.2";
-    private static final String dataLogToolVersion = "2024.3.2";
+    private static final String smartDashboardVersion = "2025.0.0-alpha-1";
+    private static final String shuffleboardVersion = "2025.0.0-alpha-2";
+    private static final String outlineViewerVersion = "2025.0.0-alpha-2";
+    private static final String robotBuilderVersion = "2025.0.0-alpha-2";
+    private static final String pathWeaverVersion = "2025.0.0-alpha-2";
+    private static final String glassVersion = "2025.0.0-alpha-2";
+    private static final String sysIdVersion = "2025.0.0-alpha-2";
+    private static final String roboRIOTeamNumberSetterVersion = "2025.0.0-alpha-2";
+    private static final String dataLogToolVersion = "2025.0.0-alpha-2";
 
 
     public abstract Property<String> getWpilibVersion();

testing/asm/build.gradle
@@ -1,7 +1,7 @@
 
 plugins {
     id "assembler"
-    id "edu.wpi.first.GradleRIO" version "2024.3.2"
+    id "edu.wpi.first.GradleRIO" version "2025.0.0-alpha-2"
 }
 
 def projectFolder = project.buildFile.parentFile

testing/asm/settings.gradle
@@ -4,7 +4,7 @@ pluginManagement {
     repositories {
         mavenLocal()
         gradlePluginPortal()
-        String frcYear = '2023'
+        String frcYear = '2025'
         File frcHome
         if (OperatingSystem.current().isWindows()) {
             String publicFolder = System.getenv('PUBLIC')

testing/cpp/build.gradle
@@ -2,7 +2,7 @@
 plugins {
     id "cpp"
     id "google-test-test-suite"
-    id "edu.wpi.first.GradleRIO" version "2024.3.2"
+    id "edu.wpi.first.GradleRIO" version "2025.0.0-alpha-2"
 }
 
 def projectFolder = project.buildFile.parentFile

testing/cpp/settings.gradle
@@ -4,7 +4,7 @@ pluginManagement {
     repositories {
         mavenLocal()
         gradlePluginPortal()
-        String frcYear = '2024'
+        String frcYear = '2025'
         File frcHome
         if (OperatingSystem.current().isWindows()) {
             String publicFolder = System.getenv('PUBLIC')

testing/java/build.gradle
@@ -1,6 +1,6 @@
 plugins {
     id "java"
-    id "edu.wpi.first.GradleRIO" version "2024.3.2"
+    id "edu.wpi.first.GradleRIO" version "2025.0.0-alpha-2"
 }
 
 def projectFolder = project.buildFile.parentFile

testing/java/settings.gradle
@@ -4,7 +4,7 @@ pluginManagement {
     repositories {
         mavenLocal()
         gradlePluginPortal()
-        String frcYear = '2024'
+        String frcYear = '2025'
         File frcHome
         if (OperatingSystem.current().isWindows()) {
             String publicFolder = System.getenv('PUBLIC')

testing/jni/build.gradle
@@ -1,7 +1,7 @@
 plugins {
     id "java"
     id "cpp"
-    id "edu.wpi.first.GradleRIO" version "2024.3.2"
+    id "edu.wpi.first.GradleRIO" version "2025.0.0-alpha-2"
     id 'edu.wpi.first.GradleJni' version '0.10.1'
 }
 

testing/jni/settings.gradle
@@ -4,7 +4,7 @@ pluginManagement {
     repositories {
         mavenLocal()
         gradlePluginPortal()
-        String frcYear = '2024'
+        String frcYear = '2025'
         File frcHome
         if (OperatingSystem.current().isWindows()) {
             String publicFolder = System.getenv('PUBLIC')

testing/kotlin/build.gradle
@@ -1,6 +1,6 @@
 plugins {
     id "org.jetbrains.kotlin.jvm" version "1.3.50"
-    id "edu.wpi.first.GradleRIO" version "2024.3.2"
+    id "edu.wpi.first.GradleRIO" version "2025.0.0-alpha-2"
 }
 
 def ROBOT_MAIN_CLASS = "frc.team0000.robot.MainKt"

testing/kotlin/settings.gradle
@@ -4,7 +4,7 @@ pluginManagement {
     repositories {
         mavenLocal()
         gradlePluginPortal()
-        String frcYear = '2023'
+        String frcYear = '2025'
         File frcHome
         if (OperatingSystem.current().isWindows()) {
             String publicFolder = System.getenv('PUBLIC')