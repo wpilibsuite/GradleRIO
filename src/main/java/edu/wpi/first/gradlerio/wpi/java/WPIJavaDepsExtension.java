40770435c29948ef3914cdc9e6000ed0cff46587
Peter Johnson <johnson.peter@gmail.com>
2024-12-15 20:03:26 -0800

    Update for 2025.1.1-beta-3 (#768)
    Bump opencv to 2025.

--- src/main/java/edu/wpi/first/gradlerio/wpi/java/WPIJavaDepsExtension.java
+++ src/main/java/edu/wpi/first/gradlerio/wpi/java/WPIJavaDepsExtension.java
@@ -56,7 +56,7 @@ public class WPIJavaDepsExtension {
         createJavaDependencies("edu.wpi.first.wpiunits", "wpiunits-java", versions.getWpilibVersion());
         createJavaDependencies("edu.wpi.first.epilogue", "epilogue-runtime-java", versions.getWpilibVersion());
 
-        createJavaDependencies("edu.wpi.first.thirdparty.frc2024.opencv", "opencv-java", versions.getOpencvVersion());
+        createJavaDependencies("edu.wpi.first.thirdparty.frc2025.opencv", "opencv-java", versions.getOpencvVersion());
         createJavaDependencies("org.ejml", "ejml-simple", versions.getEjmlVersion());
 
         createJavaDependencies("com.fasterxml.jackson.core", "jackson-annotations", versions.getJacksonVersion());
@@ -98,7 +98,7 @@ public class WPIJavaDepsExtension {
             createJniDependency("edu.wpi.first.wpimath", "wpimath-cpp", versions.getWpimathVersion(), debug, platform),
             createJniDependency("edu.wpi.first.ntcore", "ntcore-cpp", versions.getWpilibVersion(), debug, platform),
             createJniDependency("edu.wpi.first.cscore", "cscore-cpp", versions.getWpilibVersion(), debug, platform),
-            createJniDependency("edu.wpi.first.thirdparty.frc2024.opencv", "opencv-cpp", versions.getOpencvVersion(), debug, platform),
+            createJniDependency("edu.wpi.first.thirdparty.frc2025.opencv", "opencv-cpp", versions.getOpencvVersion(), debug, platform),
             createJniDependency("edu.wpi.first.wpinet", "wpinet-cpp", versions.getWpilibVersion(), debug, platform),
             createJniDependency("edu.wpi.first.wpiutil", "wpiutil-cpp", versions.getWpilibVersion(), debug, platform),
             createJniDependency("edu.wpi.first.apriltag", "apriltag-cpp", versions.getWpilibVersion(), debug, platform)
