a57b20bdacc5ad24fa7ec5bdee8c434472e1e2d3
Thad House <thadhouse1@gmail.com>
2025-03-12 22:06:50 -0700

    Disable code reboot while killing robot process

--- src/main/resources/frcKillRobot.sh
+++ src/main/resources/frcKillRobot.sh
@@ -33,6 +33,8 @@ fi
 
 printf "%s - Killing robot code in frcKillRobot.sh\n" "`date`" | FRC_ConsoleTee
 
+nirtcfg --file /etc/natinst/share/lvrt.conf --set section=LVRT,token=RTTarget.LaunchAppAtBoot,value=False
+
 if [ -e $FRC_PID_FILE ]; then
   DAEMON_PID=`cat $FRC_PID_FILE`
   if [ -e /proc/$DAEMON_PID ]; then
@@ -40,12 +42,14 @@ if [ -e $FRC_PID_FILE ]; then
     kill -term -- -$PGROUP
     sleep 1
     kill -9 -- -$PGROUP
+    sleep 1
   fi
   rm -f $FRC_PID_FILE
 fi
 
 if [ "$RESTART" = yes ]; then
    RT_STARTUP_DISABLED=`nirtcfg --get section=SYSTEMSETTINGS,token=NoApp.enabled,value=false | tr "[:upper:]" "[:lower:]"`
+   nirtcfg --file /etc/natinst/share/lvrt.conf --set section=LVRT,token=RTTarget.LaunchAppAtBoot,value=True
    if [ "$RT_STARTUP_DISABLED" = false ]; then
       killall lvrt
         else
