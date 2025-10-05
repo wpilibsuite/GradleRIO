b1473d1f41e3523d0c65b3123236ae8bf8309454
Sam Carlberg <sam@slfc.dev>
2025-10-05 19:50:19 -0400

    Exclude automatic modules

--- src/main/java/edu/wpi/first/gradlerio/wpi/WPIModulesPlugin.java
+++ src/main/java/edu/wpi/first/gradlerio/wpi/WPIModulesPlugin.java
@@ -4,21 +4,22 @@ import java.io.File;
 import java.lang.module.ModuleFinder;
 import java.net.URI;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Collectors;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.tasks.compile.JavaCompile;
 
 /**
- * Configures java compilation tasks to automatically make available every module detected on the classpath.
+ * Configures java compilation tasks to automatically make available every module detected on the classpath. Automatic
+ * modules will not be included.
  */
 public class WPIModulesPlugin implements Plugin<Project> {
   @Override
   public void apply(Project project) {
     project.getTasks().withType(JavaCompile.class).forEach(compileJava -> {
-      compileJava.doFirst((task) -> {
+      compileJava.doFirst(task -> {
         // Find all modular JAR files on the classpath, extract their module names (which may be different from
         // the names of the containing JARs!), and pass those module names to the compile task using the --add-modules
         // command line flag.
@@ -31,24 +32,39 @@ public class WPIModulesPlugin implements Plugin<Project> {
         // but still want to allow them to use `import module` statements. Thus, this plugin.
         //
         // See: https://dev.java/learn/modules/add-modules-reads/
+        var classpathJars =
+            compileJava.getClasspath().getFiles().stream()
+                .map(File::toPath)
+                .filter(p -> p.getFileName().toString().endsWith(".jar"))
+                .toList();
+
         var moduleFinder =
-            ModuleFinder.of(compileJava.getClasspath().getFiles().stream().map(File::toPath).toArray(Path[]::new));
-        var moduleNames =
-            moduleFinder.findAll().stream().map(mod -> mod.descriptor().name()).collect(Collectors.joining(","));
+            ModuleFinder.of(classpathJars.toArray(Path[]::new));
+
+        // Select only explicit modules. Automatic modules may not actually be compatible with the JPMS; notably,
+        // EJML splits packages across multiple JARs, which is not permitted by the module system.
+        var modules = moduleFinder.findAll().stream()
+            .filter(mod -> !mod.descriptor().isAutomatic())
+            .sorted(Comparator.comparing(mod -> mod.descriptor().name()))
+            .toList();
 
         project.getLogger().debug("Adding modules to the compile task `{}`:", compileJava.getName());
-        moduleFinder.findAll().stream().sorted(Comparator.comparing(mod -> mod.descriptor().name())).forEach(mod -> {
-          project.getLogger().debug("Adding module {} from {}", mod.descriptor().name(), mod.location().map(URI::toString).orElse("<unknown location>"));
+        modules.forEach(mod -> {
+          project.getLogger().debug(
+              "Adding module {} from {}",
+              mod.descriptor().name(),
+              mod.location().map(URI::toString).orElse("<unknown location>"));
         });
 
-        var compilerArgs = new ArrayList<>(compileJava.getOptions().getCompilerArgs());
-        compilerArgs.add("--module-path");
-        compilerArgs.add(compileJava.getClasspath().getAsPath());
+        var moduleNames =
+            modules.stream().map(mod -> mod.descriptor().name()).collect(Collectors.joining(","));
 
-        compilerArgs.add("--add-modules");
-        compilerArgs.add(moduleNames);
-
-        compileJava.getOptions().setCompilerArgs(compilerArgs);
+        compileJava.getOptions().getCompilerArgs().addAll(List.of(
+            "--module-path",
+            compileJava.getClasspath().getAsPath(),
+            "--add-modules",
+            moduleNames
+        ));
       });
     });
   }
