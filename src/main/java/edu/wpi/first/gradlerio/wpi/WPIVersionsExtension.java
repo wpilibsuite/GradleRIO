ba81c9472bb50bebd3f35bcea0b12c0f5f271268
Peter Johnson <johnson.peter@gmail.com>
2026-01-07 17:34:47 -0800

    Update for 2026.1.1

--- src/main/java/edu/wpi/first/gradlerio/wpi/WPIVersionsExtension.java
+++ src/main/java/edu/wpi/first/gradlerio/wpi/WPIVersionsExtension.java
@@ -6,26 +6,26 @@ import org.gradle.api.provider.Property;
 
 public abstract class WPIVersionsExtension {
 
-    private static final String wpilibVersion = "2026.1.1-beta-1";
-    private static final String niLibrariesVersion = "2026.1.0";
+    private static final String wpilibVersion = "2026.1.1";
+    private static final String niLibrariesVersion = "2026.1.2";
     private static final String opencvVersion = "4.10.0-3";
     private static final String imguiVersion = "1.89.9-1";
     private static final String ejmlVersion = "0.44.0";
     private static final String jacksonVersion = "2.19.2";
     private static final String quickbufVersion = "1.4";
-    private static final String wpimathVersion = "2026.1.1-beta-1";
+    private static final String wpimathVersion = "2026.1.1";
 
-    private static final String smartDashboardVersion = "2026.1.1-beta-1";
-    private static final String shuffleboardVersion = "2026.1.1-beta-1";
-    private static final String outlineViewerVersion = "2026.1.1-beta-1";
-    private static final String robotBuilderVersion = "2026.1.1-beta-1";
-    private static final String pathWeaverVersion = "2026.1.1-beta-1";
-    private static final String glassVersion = "2026.1.1-beta-1";
-    private static final String sysIdVersion = "2026.1.1-beta-1";
-    private static final String roboRIOTeamNumberSetterVersion = "2026.1.1-beta-1";
-    private static final String dataLogToolVersion = "2026.1.1-beta-1";
-    private static final String wpicalToolVersion = "2026.1.1-beta-1";
-    private static final String processstarterToolVersion = "2026.1.1-beta-1";
+    private static final String smartDashboardVersion = "2026.1.1";
+    private static final String shuffleboardVersion = "2026.1.1";
+    private static final String outlineViewerVersion = "2026.1.1";
+    private static final String robotBuilderVersion = "2026.1.1";
+    private static final String pathWeaverVersion = "2026.1.1";
+    private static final String glassVersion = "2026.1.1";
+    private static final String sysIdVersion = "2026.1.1";
+    private static final String roboRIOTeamNumberSetterVersion = "2026.1.1";
+    private static final String dataLogToolVersion = "2026.1.1";
+    private static final String wpicalToolVersion = "2026.1.1";
+    private static final String processstarterToolVersion = "2026.1.1";
 
 
     public abstract Property<String> getWpilibVersion();
