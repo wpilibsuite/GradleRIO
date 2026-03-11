1a6cf639971dad02357886b0dafdf0487e2a19ff
Thad House <thadhouse1@gmail.com>
2026-03-11 08:49:52 -0700

    [2027] Update to allwpilib alpha 4

--- src/main/java/org/wpilib/gradlerio/wpi/WPIVersionsExtension.java
+++ src/main/java/org/wpilib/gradlerio/wpi/WPIVersionsExtension.java
@@ -6,18 +6,18 @@ import org.gradle.api.provider.Property;
 
 public abstract class WPIVersionsExtension {
 
-    private static final String wpilibVersion = "2027.0.0-alpha-3-214-g245186cb1";
+    private static final String wpilibVersion = "2027.0.0-alpha-4";
     private static final String opencvVersion = "4.10.0-3";
     private static final String imguiVersion = "1.89.9-1";
     private static final String ejmlVersion = "0.43.1";
     private static final String jacksonVersion = "2.15.2";
     private static final String quickbufVersion = "1.3.3";
 
-    private static final String outlineViewerVersion = "2027.0.0-alpha-3-214-g245186cb1";
-    private static final String glassVersion = "2027.0.0-alpha-3-214-g245186cb1";
-    private static final String sysIdVersion = "2027.0.0-alpha-3-214-g245186cb1";
-    private static final String dataLogToolVersion = "2027.0.0-alpha-3-214-g245186cb1";
-    private static final String wpicalToolVersion = "2027.0.0-alpha-3-214-g245186cb1";
+    private static final String outlineViewerVersion = "2027.0.0-alpha-4";
+    private static final String glassVersion = "2027.0.0-alpha-4";
+    private static final String sysIdVersion = "2027.0.0-alpha-4";
+    private static final String dataLogToolVersion = "2027.0.0-alpha-4";
+    private static final String wpicalToolVersion = "2027.0.0-alpha-4";
 
     public abstract Property<String> getWpilibVersion();
     public abstract Property<String> getOpencvVersion();
