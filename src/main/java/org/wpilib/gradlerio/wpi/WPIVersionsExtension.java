dedb12af3edba043be1e97ff96ba3b8adafa97f0
Gold856 <117957790+Gold856@users.noreply.github.com>
2026-04-15 19:19:36 -0400

    Add Avaje Jsonb and remove Jackson

--- src/main/java/org/wpilib/gradlerio/wpi/WPIVersionsExtension.java
+++ src/main/java/org/wpilib/gradlerio/wpi/WPIVersionsExtension.java
@@ -9,8 +9,8 @@ public abstract class WPIVersionsExtension {
     private static final String wpilibVersion = "2027.0.0-alpha-4";
     private static final String opencvVersion = "4.10.0-3";
     private static final String imguiVersion = "1.89.9-1";
+    private static final String avajeVersion = "3.11";
     private static final String ejmlVersion = "0.43.1";
-    private static final String jacksonVersion = "2.15.2";
     private static final String quickbufVersion = "1.3.3";
 
     private static final String outlineViewerVersion = "2027.0.0-alpha-4";
@@ -23,8 +23,8 @@ public abstract class WPIVersionsExtension {
     public abstract Property<String> getOpencvVersion();
     public abstract Property<String> getImguiVersion();
 
+    public abstract Property<String> getAvajeVersion();
     public abstract Property<String> getEjmlVersion();
-    public abstract Property<String> getJacksonVersion();
     public abstract Property<String> getQuickbufVersion();
     public abstract Property<String> getOutlineViewerVersion();
     public abstract Property<String> getGlassVersion();
@@ -39,8 +39,8 @@ public abstract class WPIVersionsExtension {
         getOpencvVersion().convention(opencvVersion);
         getImguiVersion().convention(imguiVersion);
 
+        getAvajeVersion().convention(avajeVersion);
         getEjmlVersion().convention(ejmlVersion);
-        getJacksonVersion().convention(jacksonVersion);
         getQuickbufVersion().convention(quickbufVersion);
         getOutlineViewerVersion().convention(outlineViewerVersion);
         getGlassVersion().convention(glassVersion);
