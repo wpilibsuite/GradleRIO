d93e1e1c45a1ce5bcfc5b4191820d7fbaec52045
Gold856 <117957790+Gold856@users.noreply.github.com>
2026-04-26 00:47:23 -0400

    Update to native-utils 2027.7.1 and update OpenCV

--- src/main/java/org/wpilib/gradlerio/wpi/WPIVersionsExtension.java
+++ src/main/java/org/wpilib/gradlerio/wpi/WPIVersionsExtension.java
@@ -7,8 +7,7 @@ import org.gradle.api.provider.Property;
 public abstract class WPIVersionsExtension {
 
     private static final String wpilibVersion = "2027.0.0-alpha-4";
-    private static final String opencvVersion = "4.10.0-3";
-    private static final String imguiVersion = "1.89.9-1";
+    private static final String opencvVersion = "2027-4.13.0-1";
     private static final String avajeVersion = "3.11";
     private static final String ejmlVersion = "0.44.0";
     private static final String quickbufVersion = "1.4";
@@ -21,7 +20,6 @@ public abstract class WPIVersionsExtension {
 
     public abstract Property<String> getWpilibVersion();
     public abstract Property<String> getOpencvVersion();
-    public abstract Property<String> getImguiVersion();
 
     public abstract Property<String> getAvajeVersion();
     public abstract Property<String> getEjmlVersion();
@@ -37,7 +35,6 @@ public abstract class WPIVersionsExtension {
     public WPIVersionsExtension() {
         getWpilibVersion().convention(wpilibVersion);
         getOpencvVersion().convention(opencvVersion);
-        getImguiVersion().convention(imguiVersion);
 
         getAvajeVersion().convention(avajeVersion);
         getEjmlVersion().convention(ejmlVersion);
