46bf487150b3f66ca1ef10c11445c4ca2d8235b4
Peter Johnson <johnson.peter@gmail.com>
2026-05-08 00:50:05 -0500

    Update for 2026.0.0-alpha-6 (#837)

--- src/main/java/org/wpilib/gradlerio/wpi/WPIVersionsExtension.java
+++ src/main/java/org/wpilib/gradlerio/wpi/WPIVersionsExtension.java
@@ -6,17 +6,17 @@ import org.gradle.api.provider.Property;
 
 public abstract class WPIVersionsExtension {
 
-    private static final String wpilibVersion = "2027.0.0-alpha-5";
+    private static final String wpilibVersion = "2027.0.0-alpha-6";
     private static final String opencvVersion = "2027-4.13.0-3";
     private static final String avajeVersion = "3.11";
     private static final String ejmlVersion = "0.44.0";
     private static final String quickbufVersion = "1.4";
 
-    private static final String outlineViewerVersion = "2027.0.0-alpha-5";
-    private static final String glassVersion = "2027.0.0-alpha-5";
-    private static final String sysIdVersion = "2027.0.0-alpha-5";
-    private static final String dataLogToolVersion = "2027.0.0-alpha-5";
-    private static final String wpicalToolVersion = "2027.0.0-alpha-5";
+    private static final String outlineViewerVersion = "2027.0.0-alpha-6";
+    private static final String glassVersion = "2027.0.0-alpha-6";
+    private static final String sysIdVersion = "2027.0.0-alpha-6";
+    private static final String dataLogToolVersion = "2027.0.0-alpha-6";
+    private static final String wpicalToolVersion = "2027.0.0-alpha-6";
 
     public abstract Property<String> getWpilibVersion();
     public abstract Property<String> getOpencvVersion();
