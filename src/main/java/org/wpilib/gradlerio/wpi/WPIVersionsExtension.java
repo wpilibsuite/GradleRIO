1658cd1442c603cb73606d4f36dace566759ca05
Zach Harel <zach@zharel.me>
2026-06-10 21:19:25 -0400

    Update avajeVersion from 3.11 to 3.14
    matches https://github.com/wpilibsuite/allwpilib/pull/8974

--- src/main/java/org/wpilib/gradlerio/wpi/WPIVersionsExtension.java
+++ src/main/java/org/wpilib/gradlerio/wpi/WPIVersionsExtension.java
@@ -8,7 +8,7 @@ public abstract class WPIVersionsExtension {
 
     private static final String wpilibVersion = "2027.0.0-alpha-6";
     private static final String opencvVersion = "2027-4.13.0-3";
-    private static final String avajeVersion = "3.11";
+    private static final String avajeVersion = "3.14";
     private static final String ejmlVersion = "0.44.0";
     private static final String quickbufVersion = "1.4";
 
